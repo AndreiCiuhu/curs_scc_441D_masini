f8afbed70ca121f421456f97f3166d59928c41aa
vlad <vladcubic@gmail.com>
2026-05-07 15:52:07 +0300

    feature - Register BMW Blueprint

--- masini.py
+++ masini.py
@@ -1,1 +1,8 @@
+from flask import Flask
+from app.routes.bmw import bmw_bp
 
+app = Flask(__name__)
+app.register_blueprint(bmw_bp)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
