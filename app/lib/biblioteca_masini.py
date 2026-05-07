c5d40a2b084fec2c3931ee4424e91e9620cabe02
AMihneaa <asmihnea5@gmail.com>
2026-05-07 14:02:15 +0300

    feature - Extract Kia models using Ninja APIs

---
+++ app/lib/biblioteca_masini.py
@@ -0,0 +1,41 @@
+import os
+import requests
+
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY= os.getenv('API_NINJAS_KEY')
+API_URL = 'https://api.api-ninjas.com/v1/cars'
+
+MODELE_KIA = ['sportage', 'sorento', 'rio', 'stinger', 'telluride']
+
+
+def modele_kia(model: str = '') -> list:
+    params= {'make': 'Kia'}
+
+    if model:
+        params['model']= model
+
+        res= requests.get(
+            API_URL, 
+            headers= {'X-Api-Key': API_KEY}, 
+            params= params
+        )
+        if res.status_code != 200:
+            return []
+        
+        return res.json()
+
+    rezultate= []
+    for m in MODELE_KIA:
+        params['model']= m
+
+        res= requests.get(
+            API_URL, 
+            headers= {'X-Api-Key': API_KEY}, 
+            params= params
+        )
+        if res.status_code == 200 and res.json():
+            rezultate.append(res.json()[0])
+
+    return rezultate
