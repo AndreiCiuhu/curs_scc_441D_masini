eea52e306c8119c18ad03c45cc9e5d54b9f903ab
Fierea Cosmin <cosmin.fierea@stud.etti.upb.ro>
2026-05-08 00:17:01 +0300

    implementare Porsche

--- masini.py
+++ masini.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, redirect
 
 from routes.test import test_bp
+from routes.porsche import porsche_bp
 
-app= Flask(__name__)
+app = Flask(__name__)
+
 app.register_blueprint(test_bp)
+app.register_blueprint(porsche_bp)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return 'Masini'
+    return redirect("/masini/porsche/")
+
+
+@app.route("/masini")
+def masini():
+    return redirect("/masini/porsche/")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
