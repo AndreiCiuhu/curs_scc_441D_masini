e6510ff8fbb40f9be5eb32c3dd89e35782b565b8
AMihneaa <asmihnea5@gmail.com>
2026-05-07 13:24:59 +0300

    feature - Register Kia Blueprint

--- masini.py
+++ masini.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
 from routes.test import test_bp
+from routes.kia import Kia_bp
 
 app= Flask(__name__)
 app.register_blueprint(test_bp)
+app.register_blueprint(kia_bp)
 
 @app.route('/')
 def index():
