4975297d95876af7e7eb1da740df732fcaaf314d
vlad <vladcubic@gmail.com>
2026-05-07 16:10:42 +0300

    typo

--- app/test/test_biblioteca_masini.py
+++ app/test/test_biblioteca_masini.py
@@ -3,7 +3,7 @@ from app.lib.biblioteca_masini import get_culoare_bmw
 
 class TestBMW(unittest.TestCase):
     def test_culoare(self):
-        self.assertEqual(get_culoare_bmw(), "Culoarea oficială BMW M este Estoril Blue.")
+        self.assertEqual(get_culoare_bmw(), "Culoarea oficiala BMW M este albastru.")
 
 if __name__ == '__main__':
     unittest.main()
