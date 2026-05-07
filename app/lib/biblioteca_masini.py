ac1be265f8862bb87fe1bfbf589ad5207d4d9623
vlad <vladcubic@gmail.com>
2026-05-07 17:11:09 +0300

    feature - implementare integrare API Ninjas si unit teste pentru BMW

--- app/lib/biblioteca_masini.py
+++ app/lib/biblioteca_masini.py
@@ -1,26 +1,42 @@
-# Baza de date simulata cu toate detaliile necesare
-masini_db = {
-    "m3": {
-        "make": "BMW", "model": "M3", "year": 2023, "class": "compact",
-        "cylinders": 6, "displacement": 3.0, "drive": "rwd",
-        "fuel_type": "Benzina", "transmission": "Manual", "horsepower": 473
-    },
-    "m5": {
-        "make": "BMW", "model": "M5", "year": 2023, "class": "midsize",
-        "cylinders": 8, "displacement": 4.4, "drive": "awd",
-        "fuel_type": "Benzina", "transmission": "Automat", "horsepower": 600
-    },
-    "x5": {
-        "make": "BMW", "model": "X5", "year": 2023, "class": "suv",
-        "cylinders": 6, "displacement": 3.0, "drive": "awd",
-        "fuel_type": "Hibrid", "transmission": "Automat", "horsepower": 335
-    }
-}
+import os
+import requests
+from dotenv import load_dotenv
 
-def modele_bmw():
-    # Returneaza lista cu toate dictionarele de masini
-    return list(masini_db.values())
+load_dotenv()
+API_KEY = os.getenv('API_NINJAS_KEY')
+API_URL = 'https://api.api-ninjas.com/v1/cars'
 
-def detalii_bmw(model: str):
-    # Returneaza doar dictionarul masinii cerute
-    return masini_db.get(model.lower())
+# Modelele specifice pentru BMW
+MODELE_BMW = ['m3', 'm5', 'x5', 'm4', 'x7']
+
+def modele_bmw(model: str = '') -> list:
+    params = {'make': 'Bmw'}
+
+    if model:
+        params['model'] = model
+        res = requests.get(
+            API_URL, 
+            headers={'X-Api-Key': API_KEY}, 
+            params=params
+        )
+        if res.status_code != 200:
+            return []
+        return res.json()
+
+    rezultate = []
+    for m in MODELE_BMW:
+        params['model'] = m
+        res = requests.get(
+            API_URL, 
+            headers={'X-Api-Key': API_KEY}, 
+            params=params
+        )
+        if res.status_code == 200 and res.json():
+            rezultate.append(res.json()[0])
+
+    return rezultate
+
+def detalii_bmw(model: str) -> dict:
+    # Refolosim functia de mai sus pentru a lua detalii despre un singur model
+    res = modele_bmw(model=model)
+    return res[0] if res else {}
