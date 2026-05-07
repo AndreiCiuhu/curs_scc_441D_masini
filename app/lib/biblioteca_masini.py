a570a8e75ee756f8d94f298346ee2e019ab8e8aa
Ariana Ispas <arianaispas1009@yahoo.com>
2026-05-07 17:29:48 +0300

    Adauga biblioteca masini pentru Bentley

---
+++ app/lib/biblioteca_masini.py
@@ -0,0 +1,12 @@
+def culoare_bentley() -> str:
+    return (
+        "Bentley este disponibila in culori elegante precum negru, alb, gri, "
+        "albastru inchis si verde British Racing Green."
+    )
+
+
+def descriere_bentley() -> str:
+    return (
+        "Bentley este o marca britanica de automobile de lux, cunoscuta pentru "
+        "confort, eleganta, materiale premium si performanta ridicata."
+    )
