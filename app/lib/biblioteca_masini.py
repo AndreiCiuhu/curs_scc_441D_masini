67c65105135b23d1d6acddf232430a605daebad8
vlad <vladcubic@gmail.com>
2026-05-07 15:46:40 +0300

    feature - add detalii_bmw function

---
+++ app/lib/biblioteca_masini.py
@@ -0,0 +1,5 @@
+def get_culoare_bmw() -> str:
+    return "Culoarea oficiala BMW M este albastru."
+
+def get_descriere_bmw() -> str:
+    return "BMW este un producator german de automobile, cunoscut pentru performanta."
