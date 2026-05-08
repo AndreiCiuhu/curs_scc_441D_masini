845edafaa76d3a5fa313fb9ff6fc9efae0b01a1b
Viorel Maravela <viorel.maravela29@gmail.com>
2026-05-08 20:14:32 +0300

    add hyundai library functions

---
+++ app/lib/biblioteca_masini.py
@@ -0,0 +1,5 @@
+def culoare_hyundai() -> str:
+    return 'Hyundai este disponibila in culori: alb, negru, rosu.'
+
+def descriere_hyundai() -> str:
+    return 'Hyundai este un producator sud-coreean de automobile.'
