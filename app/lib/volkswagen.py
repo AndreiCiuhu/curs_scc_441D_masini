9fccf8c4e3a1ac2d84517ccd5c6075595c525ed3
Silviu <silviumarinescu16@yahoo.com>
2026-05-08 20:01:17 +0300

    Librarii

---
+++ app/lib/volkswagen.py
@@ -0,0 +1,23 @@
+modele = [
+    "Golf",
+    "Passat",
+    "Polo",
+    "Tiguan",
+    "Touareg"
+]
+
+motoare = [
+    "1.0 TSI",
+    "1.5 TSI",
+    "2.0 TSI",
+    "1.6 TDI",
+    "2.0 TDI"
+]
+
+
+def gaseste_modele_volkswagen():
+    return modele
+
+
+def gaseste_motoare_volkswagen():
+    return motoare
