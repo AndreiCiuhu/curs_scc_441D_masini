6283676f84594414a04d1acb71acc7ce95d476e9
Ciuhu <andrei.ciuhureanu@stud.etti.upb.ro>
2026-05-09 23:59:50 +0300

    modificare cale fisier testare

--- app/lib/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Package pentru biblioteca de funcții Audi."""
---
+++ app/tests/teste.py
@@ -0,0 +1,21 @@
+import unittest
+
+from app.lib.biblioteca_masini import gaseste_informatii_modele, gaseste_informatii_motor
+
+
+class TestBibliotecaMasini(unittest.TestCase):
+    def test_gaseste_informatii_modele(self):
+        modele = gaseste_informatii_modele()
+        self.assertIsInstance(modele, list)
+        self.assertIn('A3', modele)
+        self.assertIn('Q5', modele)
+
+    def test_gaseste_informatii_motor(self):
+        motor = gaseste_informatii_motor()
+        self.assertIsInstance(motor, list)
+        self.assertIn('35TFSI', motor)
+        self.assertIn('40TDI', motor)
+
+
+if __name__ == '__main__':
+    unittest.main()
