12e061a10757025ea2e174be6768d3e49af2ad3c
Andrei <robertvoinescu638@gmail.com>
2026-05-17 05:58:45 +0300

    Pagani/dev: app/routes/pagani.py - Modified routes

--- app/routes/pagani.py
+++ app/routes/pagani.py
@@ -3,18 +3,15 @@ from app.lib.biblioteca_masini import descriere_pagani, modele_pagani
 
 pagani_bp=Blueprint('pagani', __name__)
 
-@pagani_bp.route('/masini')
-def pagina_temei():
-	return "Pagina temei Masini"
 
-@pagani_bp.route('/masini/pagani')
+@pagani_bp.route('/pagani')
 def pagina_pagani():
 	return render_template('pagani/pagani.html')
 
-@pagani_bp.route('/masini/pagani/modele')
+@pagani_bp.route('/pagani/modele')
 def culoare():
-	return render_template('pagani/modele.html', modele=modele_pagani())
+	return render_template(modele_pagani())
 
-@pagani_bp.route('/masini/pagani/descriere')
+@pagani_bp.route('/pagani/descriere')
 def descriere():
-	return render_template('pagani/descriere.html', descriere=descriere_pagani())
+	return render_template(descriere_pagani())
