bd0ec42d738a9211dd6998cd4903d02502f58b87
matheo <moisanu_mateo@yahoo.com>
2026-05-17 20:26:48 +0300

    register blueprint for endpoint ferrari

--- masini.py
+++ masini.py
@@ -1,9 +1,10 @@
 from flask import Flask
-
+from app.routes.ferrari import ferrari_bp 
 from routes.test import test_bp
 
 app= Flask(__name__)
-app.register_blueprint(test_bp)
+app.register_blueprint(ferrari_bp)
+
 
 @app.route('/')
 def index():
