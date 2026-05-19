25b474f4374b8b86fc2b95f22224e9c7e0f434e5
tiberiudragomir <dragomirtiberiu22@yahoo.com>
2026-05-19 06:22:24 -0700

    Fix biblioteca_masini path

---
+++ app/lib/biblioteca_masini.py
@@ -0,0 +1,6 @@
+def culoare_dacia() -> str:
+    return 'Dacia este disponibil in culorile: alb ca varul, rosu ca macul si negru ca taciunele'
+
+def descriere_dacia()-> str:
+    return 'Dacia este un producator roman de automobile.'
+
